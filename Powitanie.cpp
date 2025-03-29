d8c277c24b199287d7e8a8c0e93c95fa10f67d78
gientki <74157161+gientki@users.noreply.github.com>
2025-03-29 22:49:04 +0100

    Create Powitanie.cpp

---
+++ Powitanie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int x,y;
+	cin>>x;
+	cin>>y;
+	cout<<"Witaj na V Konkursie FRAKTAL! Suma "<<x<<" i "<<y<<" to "<<x+y<<". Powodzenia, programisto!";
+
+	return 0;
+}
